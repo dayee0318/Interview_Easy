2f468411ec036f0fbf6a3f4164c5aeccc81a7a3f
dayee0318 <dy0318m@gmail.com>
2023-03-30 18:02:19 -0600

    Final version.

--- src/ReverseString_344.java
+++ src/ReverseString_344.java
@@ -1,5 +1,6 @@
 public class ReverseString_344 {
     public static void reverseString1(char[] s) {
+        // 내가 처음 쓴 답
         char[] r = new char[s.length];
         int j = 0;
         for (int i = s.length-1; i >= 0; i--) {
@@ -11,17 +12,28 @@ public class ReverseString_344 {
     }
 
     public static void reverseString2(char[] s) {
-        int i = 0;
-        int j = s.length-1;
-        while (i < j) {
-            char temp = s[i];   // s[0] 저장
-            s[i] = s[j];        // 저장해둔 자리에 뒤에있던 걸 옮기고
-            s[j] = temp;        // 저장해둔걸 뒤로 옮겨서 서로 자리를 바꿈.
+        // 정답
+        int left = 0;
+        int right = s.length-1;
+        while (left < right) {
+            char temp = s[left];   // s[0] 저장
+            s[left] = s[right];        // 저장해둔 자리에 뒤에있던 걸 옮기고
+            s[right] = temp;        // 저장해둔걸 뒤로 옮겨서 서로 자리를 바꿈.
 
-            i++;       // 그리고 i는 증가시키고
-            j--;       // j는 감소시켜서 중간에서 만날때까지 반복
+            left++;       // 그리고 i는 증가시키고
+            right--;       // j는 감소시켜서 중간에서 만날때까지 반복
         }
     }
+
+    public static void reverseString3(char[] s) {
+        // for loop 버전
+        for (int left = 0, right = s.length-1; left <= right; left++, right--) {
+            char temp = s[left];   // s[0] 저장
+            s[left] = s[right];        // 저장해둔 자리에 뒤에있던 걸 옮기고
+            s[right] = temp;        // 저장해둔걸 뒤로 옮겨서 서로 자리를 바꿈.
+        }
+    }
+
     public static void main(String[] args) {
         char[] input = new char[] {'h', 'e', 'l', 'l', 'o'};
         reverseString1(input);
